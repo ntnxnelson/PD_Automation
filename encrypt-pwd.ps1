248646704c94fdffd7ac4902e413e41451265ea4
Chris Nelson <chrisnelson@CJNMCBKPRO.local>
2018-08-26 12:17:19 -0400

    reset commit

--- encrypt-pwd.ps1
+++
@@ -1,5 +0,0 @@
-﻿$PasswordFile = "c:\temp\Password.txt"
-$KeyFile = "c:\temp\AES.key"
-$Key = Get-Content $KeyFile
-$Password = "Nutanix/4u!" | ConvertTo-SecureString -AsPlainText -Force
-$Password | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
--- generate_key.ps1
+++
@@ -1,4 +0,0 @@
-﻿$KeyFile = "c:\temp\AES.key"
-$Key = New-Object Byte[] 32   # You can use 16, 24, or 32 for AES
-[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
-$Key | out-file $KeyFile
