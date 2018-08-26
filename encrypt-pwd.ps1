ae8eb11a3d7cf6b40f2ac324af7957cf44568fa4
Chris Nelson <chrisnelson@CJNMCBKPRO.local>
2018-08-26 12:07:54 -0400

    initial commit

---
+++ encrypt-pwd.ps1
@@ -0,0 +1,5 @@
+﻿$PasswordFile = "c:\temp\Password.txt"
+$KeyFile = "c:\temp\AES.key"
+$Key = Get-Content $KeyFile
+$Password = "Nutanix/4u!" | ConvertTo-SecureString -AsPlainText -Force
+$Password | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
---
+++ setcred.ps1
@@ -0,0 +1,6 @@
+﻿$User = "chris@nemo.corp"
+$PasswordFile = "C:\temp\Password.txt"
+$KeyFile = "C:\temp\AES.key"
+$key = Get-Content $KeyFile
+$MyCredential = New-Object -TypeName System.Management.Automation.PSCredential `
+ -ArgumentList $User, (Get-Content $PasswordFile | ConvertTo-SecureString -Key $key)
