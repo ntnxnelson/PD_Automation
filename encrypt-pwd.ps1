292740e13e1514596d7a20d2fb0a6b4cceb2761c
unknown <chris.nelson@nutanix.com>
2018-08-28 05:20:05 -0700

    added secure password support files

---
+++ encrypt-pwd.ps1
@@ -0,0 +1,5 @@
+﻿$PasswordFile = "c:\temp\Password.txt"
+$KeyFile = "c:\temp\AES.key"
+$Key = Get-Content $KeyFile
+$Password = "enter password here" | ConvertTo-SecureString -AsPlainText -Force
+$Password | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
